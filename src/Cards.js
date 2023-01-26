b51055736ee29a0d5ca78a690a235be66c24dbc7
Jeff Sparkes <jsparkes@gmail.com>
2023-01-26 01:42:17 -0500

    WIP.  Basic syntax errors eliminated.

---
+++ src/Cards.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export function shuffleArray(array) {
+  return function () {
+    return array
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  };
+}
